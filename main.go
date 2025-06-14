e8058fb0ef87bda5e818d097a7e4d4a3c1267491
sathirak <145209193+sathirak@users.noreply.github.com>
2025-06-15 00:04:10 +0530

    feat: enhance action inputs and update main functionality to support optional registry credentials

--- main.go
+++ main.go
@@ -4,31 +4,47 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/sethvargo/go-githubactions"
 )
 
 func main() {
-
+	registry := githubactions.GetInput("registry")
 	repoName := githubactions.GetInput("name")
 	if repoName == "" {
 		githubactions.Fatalf("missing 'name'")
 	}
 
-	ref, err := name.NewRepository(repoName)
+	username := githubactions.GetInput("username")
+	password := githubactions.GetInput("password")
+
+	fullRepo := repoName
+	if registry != "" {
+		fullRepo = fmt.Sprintf("%s/%s", registry, repoName)
+	}
+
+	ref, err := name.NewRepository(fullRepo)
 	if err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 
-    tags, err := remote.List(ref)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var opts []remote.Option
+	if username != "" && password != "" {
+		auth := &authn.Basic{
+			Username: username,
+			Password: password,
+		}
+		opts = append(opts, remote.WithAuth(auth))
+	}
 
-    for _, tag := range tags {
-        fmt.Println(tag)
-    }
+	tags, err := remote.List(ref, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println("Hello, world!")
+	for _, tag := range tags {
+		fmt.Println(tag)
+	}
 }
